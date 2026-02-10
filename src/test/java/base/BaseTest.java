4f21bfa9afab387e7d4947ce029c72e46f97c056
resh-re <reshma.holla2093@gmail.com>
2026-02-09 16:12:51 -0800

    Create BaseTest.java

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,20 @@
+package base;
+
+import config.ConfigLoader;
+import driver.DriverFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        ConfigLoader.load();
+        DriverFactory.initDriver();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+}
